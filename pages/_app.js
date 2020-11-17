00873cfa1513757332af4a69c39c6500e77502ec
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-11-18 02:45:39 +0700

    :tada: Intail Project 🌻✌

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import '../core/styles/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React, { useEffect } from 'react'
+
+import Head from 'next/head'
+
+function App({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Hello Next</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import HomePage from '../components/HomePage'
+
+export default function Home() {
+  return (
+    <HomePage />
+  );
+}
