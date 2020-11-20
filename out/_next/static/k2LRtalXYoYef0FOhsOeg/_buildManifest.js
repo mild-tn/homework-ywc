0bfa9f0f9a490f5cd0d8f1b16a526f1d42d2fb21
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-11-20 23:37:11 +0700

    Deploy Next.js to gh-pages

--- out/_next/static/k2LRtalXYoYef0FOhsOeg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-bfd1701eb284724b82ac.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-68532ee28cc0e349045e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
