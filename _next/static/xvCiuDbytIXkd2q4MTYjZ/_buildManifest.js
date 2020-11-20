99fc4a2af91a7499091ffc0560b8c2e2479dc8b6
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-11-20 22:38:03 +0700

    Deploy gh-pages

---
+++ _next/static/xvCiuDbytIXkd2q4MTYjZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-365963ab052c7134da74.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1cb9f94df4ca009dcc35.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
