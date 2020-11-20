acb3321e67a0b4d08d05206ca85f2054bbbf54c5
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-11-20 22:04:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        IMB: ['IBM Plex Sans Thai Normal', 'IBM Plex Sans Thai Bold'],
+        body: ['IBM Plex Sans Thai Normal']
+      },
+      colors: {
+        primary: "#27397c"
+      },
+      fontSize: {
+        'heading5': '24px'
+      },
+    },
   },
   variants: {},
   plugins: [],
